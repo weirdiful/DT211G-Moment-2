5d100bfede318ad6bae4f48b5a3f9c1d9bf917f3
johannahyll <johanna.hyll@gmail.com>
2025-02-13 12:37:33 +0100

    json problem

--- dist/index.e82f28a0.js
+++ dist/index.e82f28a0.js
@@ -597,61 +597,57 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"dV6cC":[function(require,module,exports,__globalThis) {
 "use strict";
-// Hämta modalen
-let modal = document.getElementById("myModal");
-let modalImg = document.getElementById("img01");
-let captionText = document.getElementById("caption");
-// Hämta alla bilder med klassen "myImg"
-let images = document.querySelectorAll(".myImg, .myImg img");
-// Lägg till klick-händelse på varje bild
-images.forEach(function(img) {
-    img.onclick = function() {
-        modal.style.display = "block";
-        if (this.tagName.toLowerCase() == "picture") {
-            modalImg.src = this.querySelector("img").src;
-            captionText.innerHTML = this.querySelector("img").alt;
-        } else {
-            modalImg.src = this.src;
-            captionText.innerHTML = this.alt;
-        }
-    };
-});
-// Stäng modalen vid klick på stängningsknappen
-let span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-};
-// Stäng modalen om användaren klickar utanför bilden
-modal.onclick = function(event) {
-    if (event.target === modal) modal.style.display = "none";
-};
-function openNav() {
-    document.getElementById("menu").style.width = "250px";
-}
-function closeNav() {
-    document.getElementById("menu").style.width = "0";
-}
+// // Hämta modalen
+// let modal = document.getElementById("myModal");
+// let modalImg = document.getElementById("img01");
+// let captionText = document.getElementById("caption");
+// // Hämta alla bilder med klassen "myImg"
+// let images = document.querySelectorAll(".myImg, .myImg img");
+// // Lägg till klick-händelse på varje bild
+// images.forEach(function(img) {
+//     img.onclick = function() {
+//         modal.style.display = "block";
+//         if (this.tagName.toLowerCase() == "picture") {
+//            modalImg.src = this.querySelector("img").src;
+//           captionText.innerHTML = this.querySelector("img").alt;
+//         } else {
+//         modalImg.src = this.src;
+//         captionText.innerHTML = this.alt;
+//       }
+//     };
+// });
+// // Stäng modalen vid klick på stängningsknappen
+// let span = document.getElementsByClassName("close")[0];
+// span.onclick = function() {
+//     modal.style.display = "none";
+// };
+// // Stäng modalen om användaren klickar utanför bilden
+// modal.onclick = function(event) {
+//     if (event.target === modal) {
+//         modal.style.display = "none";
+//     }
+// };
 document.addEventListener("DOMContentLoaded", ()=>{
-    fetch('.data/courses.json').then((response)=>{
+    fetch('courses.json').then((response)=>{
         if (!response.ok) throw new Error("N\xe4tverksfel vid h\xe4mtning");
         return response.json();
     }).then((data)=>{
         displayCourses(data);
     }).catch((error)=>console.error("Fel vid h\xe4mtning:", error));
 });
-// Funktion för att visa kurser på sidan
+// Funktion för att visa kurser i en tabell
 function displayCourses(courses) {
     const courseContainer = document.getElementById("course-list");
     courseContainer.innerHTML = "";
     courses.forEach((course)=>{
-        const courseElement = document.createElement("div");
-        courseElement.classList.add("course");
-        courseElement.innerHTML = `
-          <h3>${course.coursename} (${course.code})</h3>
-          <p>Progression: ${course.progression}</p>
-          <a href="${course.syllabus}" target="_blank">Kursplan</a>
+        const row = document.createElement("tr");
+        row.innerHTML = `
+          <td>${course.coursename}</td>
+          <td>${course.code}</td>
+          <td>${course.progression}</td>
+          <td><a href="${course.syllabus}" target="_blank">Kursplan</a></td>
       `;
-        courseContainer.appendChild(courseElement);
+        courseContainer.appendChild(row);
     });
 }
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,48 @@
 "use strict";
 
-// Hämta modalen
-let modal = document.getElementById("myModal");
-let modalImg = document.getElementById("img01");
-let captionText = document.getElementById("caption");
+// // Hämta modalen
+// let modal = document.getElementById("myModal");
+// let modalImg = document.getElementById("img01");
+// let captionText = document.getElementById("caption");
 
-// Hämta alla bilder med klassen "myImg"
-let images = document.querySelectorAll(".myImg, .myImg img");
+// // Hämta alla bilder med klassen "myImg"
+// let images = document.querySelectorAll(".myImg, .myImg img");
 
-// Lägg till klick-händelse på varje bild
-images.forEach(function(img) {
-    img.onclick = function() {
-        modal.style.display = "block";
+// // Lägg till klick-händelse på varje bild
+// images.forEach(function(img) {
+//     img.onclick = function() {
+//         modal.style.display = "block";
 
-        if (this.tagName.toLowerCase() == "picture") {
-          modalImg.src = this.querySelector("img").src;
-          captionText.innerHTML = this.querySelector("img").alt;
-        } else {
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
-    };
-});
+//         if (this.tagName.toLowerCase() == "picture") {
+//            modalImg.src = this.querySelector("img").src;
+//           captionText.innerHTML = this.querySelector("img").alt;
+//         } else {
+//         modalImg.src = this.src;
+//         captionText.innerHTML = this.alt;
+//       }
+//     };
+// });
 
-// Stäng modalen vid klick på stängningsknappen
-let span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-};
+// // Stäng modalen vid klick på stängningsknappen
+// let span = document.getElementsByClassName("close")[0];
+// span.onclick = function() {
+//     modal.style.display = "none";
+// };
 
-// Stäng modalen om användaren klickar utanför bilden
-modal.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-};
+// // Stäng modalen om användaren klickar utanför bilden
+// modal.onclick = function(event) {
+//     if (event.target === modal) {
+//         modal.style.display = "none";
+//     }
+// };
 
 
 
-function openNav() {
-  document.getElementById("menu").style.width = "250px";
 
-}
-
-function closeNav() {
-  document.getElementById("menu").style.width = "0";
-}
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  fetch('.data/courses.json')
+  fetch('courses.json')  
       .then(response => {
           if (!response.ok) {
               throw new Error("Nätverksfel vid hämtning");
@@ -62,20 +55,19 @@ document.addEventListener("DOMContentLoaded", () => {
       .catch(error => console.error("Fel vid hämtning:", error));
 });
 
-// Funktion för att visa kurser på sidan
+// Funktion för att visa kurser i en tabell
 function displayCourses(courses) {
   const courseContainer = document.getElementById("course-list");
   courseContainer.innerHTML = "";
 
   courses.forEach(course => {
-      const courseElement = document.createElement("div");
-      courseElement.classList.add("course");
-      courseElement.innerHTML = `
-          <h3>${course.coursename} (${course.code})</h3>
-          <p>Progression: ${course.progression}</p>
-          <a href="${course.syllabus}" target="_blank">Kursplan</a>
+      const row = document.createElement("tr");
+      row.innerHTML = `
+          <td>${course.coursename}</td>
+          <td>${course.code}</td>
+          <td>${course.progression}</td>
+          <td><a href="${course.syllabus}" target="_blank">Kursplan</a></td>
       `;
-      courseContainer.appendChild(courseElement);
+      courseContainer.appendChild(row);
   });
 }
-
